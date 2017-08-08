241b6caec7bd5d85ed7b11911dd0079c9e0106f7
Dzmitry Kaliukhovich <dzmitry.kaliukhovich@gmail.com>
2017-08-08 14:10:59 +0200

    Add files via upload

--- scripts/retrieve_behavioural_data.m
+++ scripts/retrieve_behavioural_data.m
@@ -34,7 +34,7 @@ function trial_data = compute_velocity(trial_data)
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % Bits 7 x 6 x 5 x 4 x 3 x 2 x 1 x 0:
-        %      - x - x - x V x V x V x V x S,
+        %      - x V x V x V x V x V x V x S,
         % where V could be 0 or 1 and code displacement w.r.t. the previous
         % timestamp, and S codes the direction of displacement (clockwise
         % or counter-clockwise).
@@ -77,8 +77,8 @@ function trial_data = compute_velocity(trial_data)
         trial_data(trial_no).velocity = [adj_displacement(:, 1) adj_displacement(:, 2) / time_step];
         
         % For debugging purposes only. Comment when no needed.
-        dt = trial_data(trial_no).velocity(2:end, 1) - trial_data(trial_no).velocity(1:end - 1, 1);
-        fprintf('Trial #%d: min x mean x median x max = %.6f x %.6f x %.6f x %.6f\n', trial_no, min(dt), mean(dt), median(dt), max(dt));
+        % dt = trial_data(trial_no).velocity(2:end, 1) - trial_data(trial_no).velocity(1:end - 1, 1);
+        % fprintf('Trial #%d: min x mean x median x max = %.6f x %.6f x %.6f x %.6f\n', trial_no, min(dt), mean(dt), median(dt), max(dt));
         
     end
     
@@ -87,7 +87,7 @@ end
 function trial_data = split_per_trial(raw_data)
 
     % All data are stored per trial.
-    trial_data = [];
+    trial_data = struct([]);
 
     % Beginning of a trial (port identifier = 1 x proper label).
     header_start = find(raw_data(:, 2) == 1 & raw_data(:, 3) == 254);    
@@ -108,10 +108,12 @@ function trial_data = split_per_trial(raw_data)
         end
         
         % Retrieve experiment type and condition number.
-        indices = find(raw_data(curr_index + 1:header_stop - 1, 2) == 1 & raw_data(curr_index + 1:header_stop - 1, 3));
-        values = raw_data(curr_index + indices, 3);
+        indices = find(raw_data(curr_index + 1:header_stop - 1, 2) == 1 & raw_data(curr_index + 1:header_stop - 1, 3)) + curr_index;
+        values = raw_data(indices, 3);
         if length(values) ~= 2
-            error('Failed to retrieve experiment type and/or condition number!');
+            fprintf('Skipping a trial with corrupted header!\n');
+            disp(raw_data(indices, :));
+            continue;
         end
         
         % Retrieve photocell events.
@@ -123,27 +125,15 @@ function trial_data = split_per_trial(raw_data)
         displacement = raw_data(indices, [1, 3]);
         
         % Adding a new trial.
-        trial_data(counter).start        = raw_data(curr_index, 1);
-        trial_data(counter).header_stop  = raw_data(header_stop, 1);
-        trial_data(counter).stop         = raw_data(trial_stop, 1); 
-        trial_data(counter).exp_type     = values(1);
-        trial_data(counter).condition    = values(2);
-        trial_data(counter).photocell    = photocell;
-        trial_data(counter).displacement = displacement;
+        trial_data(end + 1).start    = raw_data(curr_index, 1);
+        trial_data(end).header_stop  = raw_data(header_stop, 1);
+        trial_data(end).stop         = raw_data(trial_stop, 1); 
+        trial_data(end).exp_type     = values(1);
+        trial_data(end).condition    = values(2);
+        trial_data(end).photocell    = photocell;
+        trial_data(end).displacement = displacement;
         
     end
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    stim_dur = [];
-    for counter = 1:length(trial_data)
-        stim_dur(end + 1) = trial_data(counter).photocell(2) - trial_data(counter).photocell(1);
-    end
-    
-    figure;
-    plot(1000.0 * stim_dur), xlabel('trial no.'), ylabel('stimulus presentation, msec');
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
 end
 
