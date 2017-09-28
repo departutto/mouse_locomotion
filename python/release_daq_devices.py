4db0b742aa0e87ad42d330c94ba96b2308fed15a
Dzmitry Kaliukhovich <dzmitry.kaliukhovich@gmail.com>
2017-09-28 19:00:08 +0200

    Delete release_daq_devices.py

--- python/release_daq_devices.py
+++
@@ -1,9 +0,0 @@
-from mcculw import ul
-
-# Maximum number of DAQ devices allowed to be registered in the system.
-MAX_DEVICES = 100
-
-for counter in range(MAX_DEVICES):
-    ul.release_daq_device(counter)
-
-print('All DAQ devices and resources associated with them have been released.')
