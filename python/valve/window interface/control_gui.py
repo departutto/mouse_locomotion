75e5145d3f4a570267f8acffae136dfe5d7e1102
Dzmitry Kaliukhovich <dzmitry.kaliukhovich@gmail.com>
2018-05-15 17:24:36 +0200

    Delete control_gui.py

--- python/valve/window interface/control_gui.py
+++
@@ -1,7 +0,0 @@
-import window_interface, relay_interface
-
-if __name__ == '__main__':
-    
-    valve = relay_interface.USB_ERB08_single_port(relay_num = 7)
-    app = window_interface.User_window(valve)
-    app.mainloop()
